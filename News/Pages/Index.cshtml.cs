ce3dec05ca2ffd6e5c6a4a708859c814c8f1407f
Gilbert Mule <manstill09@outlook.com>
2022-06-09 20:43:24 +0300

    added news api url and key.

--- News/Pages/Index.cshtml.cs
+++ News/Pages/Index.cshtml.cs
@@ -1,20 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using News.Services;
 
 namespace News.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly INewsService _newsService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, INewsService newsService)
         {
             _logger = logger;
+            _newsService = newsService;
         }
 
         public void OnGet()
         {
-
+            _newsService.GetFinanceNews();
         }
     }
 }
--- News/Program.cs
+++ News/Program.cs
@@ -1,7 +1,10 @@
+using News.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<INewsService, NewsService>();
 
 var app = builder.Build();
 
